b259c25268fec16413960cdfbc4b28526f5b6755
GoncalvesF2 <filipe.goncalves@ppb.com>
2024-11-29 13:03:35 +0000

    Dummy test

---
+++ mapStruct/src/test/java/com/AppTest.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AppTest {
+
+    @Test
+    void testMain() {
+        assertTrue(true);
+    }
+}
